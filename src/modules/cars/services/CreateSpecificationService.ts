ea8bfe844d2a881b7e7c2f1bf6eb6ca2a3e14e93
Giovani Lima <giovanilima775@hotmail.com>
2021-11-08 20:08:29 -0300

    feat: create specifications

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,25 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+import { Specification } from '../model/Specification';
+interface IRequest {
+    name: string;
+    description: string;
+}
 class CreateSpecificationService {
-  execute() {
-    console.log('todo');
-  }
+
+    constructor(private specificationsRepository: ISpecificationsRepository) {
+
+    }
+    execute({name, description}: IRequest): void {
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error('Specification already exists');
+        }
+
+        this.specificationsRepository.create({ name, description });
+    }
+
+
 }
 
 export { CreateSpecificationService };
