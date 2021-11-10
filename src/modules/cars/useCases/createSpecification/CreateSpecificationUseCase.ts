88647f3d1bff9b4c8a466f676b325da40173eb59
Giovani Lima <giovanilima775@hotmail.com>
2021-11-10 18:40:12 -0300

    refactor: add create specifications UseCase and Controller

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,21 @@
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
+import { Specification } from '../../model/Specification';
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+class CreateSpecificationUseCase {
+
+    constructor(private specificationsRepository: ISpecificationsRepository) {
+
+    }
+    execute({name, description}: IRequest): void {
+
+        this.specificationsRepository.create({ name, description });
+    }
+
+
+}
+
+export { CreateSpecificationUseCase };
