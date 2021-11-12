dc790d687251da53cbf9b9b2c075d9696f02517a
Giovani Lima <giovanilima775@hotmail.com>
2021-11-12 18:53:39 -0300

    docs: add to swagger get and post categories route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 import { router } from './routes/index';
+import swaggerUi from 'swagger-ui-express';
 const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
