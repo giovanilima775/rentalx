255f59429928944ceb1c48876c7b4e8a52a0ffc6
Giovani Lima <giovanilima775@hotmail.com>
2021-11-15 05:01:49 -0300

    refactor: Create category using tsyringe

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,14 +1,14 @@
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
-import { CreateCategoryController } from './CreateCategoryController';
-import { CreateCategoryUseCase } from './createCategoryUseCase';
+// import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+// import { CreateCategoryController } from './CreateCategoryController';
+// import { CreateCategoryUseCase } from './createCategoryUseCase';
 
-export default (): CreateCategoryController => {
-    const categoriesRepository = new CategoriesRepository();
-    const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-    const createCategoryController = new CreateCategoryController(createCategoryUseCase);
+// export default (): CreateCategoryController => {
+//     const categoriesRepository = new CategoriesRepository();
+//     const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+//     const createCategoryController = new CreateCategoryController(createCategoryUseCase);
 
-    return createCategoryController;
-}
+//     return createCategoryController;
+// }
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import "./database";
+import "./shared/container";
+
 import { router } from './routes/index';
 import swaggerUi from 'swagger-ui-express';
-import "./database"
-const app = express();
-app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
-app.use(router);
 
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require('./swagger.json')));
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server running on port 3333'));
