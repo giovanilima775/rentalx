d5081843b1454f2b1a73f79e073eaa49d18b2d15
Giovani Lima <giovanilima775@hotmail.com>
2021-11-17 21:21:47 -0300

    test: create category test

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -4,6 +4,7 @@ export class AppError extends Error {
     public readonly statusCode: number;
 
     constructor(message: string, statusCode = 400) {
+        super()
         this.message = message;
         this.statusCode = statusCode;
     }
