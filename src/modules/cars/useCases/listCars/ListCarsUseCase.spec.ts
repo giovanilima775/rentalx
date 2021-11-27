7fa3c3ebab7179a94fba0a2a04702f512bb8532f
Giovani Lima <giovanilima775@hotmail.com>
2021-11-27 19:24:33 -0300

    test: list all available cars by name

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -38,7 +38,23 @@ describe("Create car", () => {
         });
 
         const cars = await listCarsUseCase.execute({ brand: "Car_brand_test" });
-        console.log(cars);
+
+        expect(cars).toEqual([car]);
+    })
+
+    it("should be able to list all available cars by name", async () => {
+        const car = await carsRepositoryInMemory.create({
+            name: "Car2",
+            description: "Car description",
+            daily_rate: 110.0,
+            license_plate: "DEF-1234",
+            fine_amount: 40,
+            brand: "Car_brand_test",
+            category_id: "category_id",
+        });
+
+        const cars = await listCarsUseCase.execute({ name: "Car2" });
+
         expect(cars).toEqual([car]);
     })
 })
